b7cfc6e5af996a34e8560a55a83cf4ae8b4dca0c
Milica2000 <mimche00@gmail.com>
2022-04-02 17:02:36 +0200

    Updated

---
+++ Assets/Scripts/BaseCharacter.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BaseCharacter : MonoBehaviour, IDamageable
+{   
+    private int health;
+    public int Health { get { return health; } }
+    public void SetHealth(int h) { health = h; }
+    
+    public void TakeDamage(int damage){
+        health -= damage;
+        if (health <= 0)
+        {
+            Die();
+        }
+    }
+    // gameObject je referenca na objekat na kojem je zakacena skripta
+    public void Die(){
+        Destroy(gameObject);
+    }
+
+    
+}
